2389986006adda824f2b3c6dbe12f5ae9665e9d8
lanly_dev <lanly-dev@users.noreply.github.com>
2021-11-14 02:55:41 -0800

    wip: draw times

--- client/components/Graph.vue
+++ client/components/Graph.vue
@@ -40,24 +40,25 @@ onBeforeMount(async () => {
   //   console.log(error)
   // }
   data = await fetch('/api/data').then((s) => s.json())
-  // console.log(data)
   makePies(data)
 })
 
 function makePies(data) {
+  data = normalizeData(data)
   const width = 500
   const height = 500
   const margin = 40
   const donutWidth = 10
   const donutCenter = 10
+  // console.log(data)
   let svg = d3.select(graph.value).append('svg').attr('width', width).attr('height', height)
-  for (let threadCount = 0; threadCount < data.length; threadCount++) {
+  for (let threadCount = 0; threadCount < data.times.length; threadCount++) {
     const radius = Math.min(width, height) / 2 - donutWidth * threadCount - margin
     const color = d3.scaleOrdinal().range(d3.schemeDark2)
     // const color = d3.scaleOrdinal().domain(['a', 'b', 'c', 'd']).range(['red', 'green', 'blue', 'white'])
 
-    const dummyData = { a: 50, b: 50 }
-    console.log('hello')
+    const timesData = data.times[threadCount]
+
     svg.append('g')
 
     // Compute the position of each group on the pie:
@@ -68,7 +69,7 @@ function makePies(data) {
       .value((d) => d[1])
       .sort(null)
     //@ts-ignore
-    const data_ready = pie(Object.entries(dummyData))
+    const pieData = pie(timesData)
 
     const innerRadius = donutWidth
     const arc = d3
@@ -78,7 +79,7 @@ function makePies(data) {
 
     svg
       .selectAll('hello')
-      .data(data_ready)
+      .data(pieData)
       .enter()
       .append('path')
       .attr('d', arc)
@@ -87,6 +88,17 @@ function makePies(data) {
       .style('stroke-width', '2px')
       .style('opacity', 0.7)
       .attr('transform', `translate(${width / 2},${height / 2})`)
+  }
+  //@ts-ignore
+  function normalizeData(data) {
+    const { model, speed } = data[0]
+    const nData = { model, speed, times: <any>[] }
+    for (const d of data) {
+      const temp = []
+      for (const type in d.times) temp.push([type, d.times[type]])
+      nData.times.push(temp)
+    }
+    return nData
   }
 }
 </script>
